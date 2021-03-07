a30329cb43e1139dfe97502aedc4ae6e62f46eca
Denis-Grekov <80217877+Denis-Grekov@users.noreply.github.com>
2021-03-07 21:27:55 +0300

    Add files via upload

---
+++ num1.js
@@ -0,0 +1,24 @@
+function first (num){
+    for (let i=1; i<=num; i++){
+        
+        
+            switch (i) {
+                case 5 : console.log("Пять");
+                break;
+                case 13 : console.log ("Тринадцать");
+                break;
+                case 22 : console.log ("Двадцать два");
+                break;                
+                case 35 : console.log ("Тридцать пять");
+                break;
+                case 98 : console.log ("Девяносто восемь");
+                break;
+                default:  console.log(i);
+                break;
+                
+                
+            }
+            
+        }
+    }
+    
